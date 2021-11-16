6807064ebd78edeebcc4f325481be089061e5121
Андрей Разумов <alexkirillof@yandex.ru>
2021-11-16 22:59:27 +0300

    Lesson_3

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import { images } from "./img";
-import { audioFiles } from "./media";
-import { videoFiles } from "./media";
-import './pages/Render.js';
-import '../scss/main.scss';
-import '../css/main.css';
